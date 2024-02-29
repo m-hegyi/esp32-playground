c183243e2b36465d3196e1b2746ea0b74e4774ea
Márió Hegyi <mario.hegyi93@gmail.com>
2024-02-29 10:44:58 +0100

    blink test

--- main/main.c
+++ main/main.c
@@ -1,9 +1,21 @@
 #include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-#define CURRENT_PIN GPIO_NUM_3
+#define OUTPUT_GPIO GPIO_NUM_3
+#define INPUT_GPIO GPIO_NUM_5
 
 void app_main(void)
 {
-    gpio_set_direction(CURRENT_PIN, GPIO_MODE_OUTPUT);
-    gpio_set_level(CURRENT_PIN, 1);
+    gpio_set_direction(OUTPUT_GPIO, GPIO_MODE_OUTPUT);
+    // gpio_set_direction(INPUT_GPIO, GPIO_MODE_INPUT);
+
+    bool light_state = false;
+
+    while (true)
+    {
+        gpio_set_level(OUTPUT_GPIO, light_state);
+        light_state = !light_state;
+        vTaskDelay(100);
+    }
 }
