2096bea53e3f71eb39ed63dfde966ec46403d9bc
Márió Hegyi <mario.hegyi93@gmail.com>
2024-02-29 10:18:27 +0100

    ignore cache and basic high output

--- main/main.c
+++ main/main.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
+#include "driver/gpio.h"
+
+#define CURRENT_PIN GPIO_NUM_3
 
 void app_main(void)
 {
-    printf("Hello World!");
+    gpio_set_direction(CURRENT_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(CURRENT_PIN, 1);
 }
