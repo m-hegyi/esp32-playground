67340b731abcb8a7bad56a8a6231d37dfc5d8d94
Márió Hegyi <mario.hegyi93@gmail.com>
2024-02-29 12:13:13 +0100

    using interrupt to change led state

--- main/main.c
+++ main/main.c
@@ -1,21 +1,64 @@
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 
-#define OUTPUT_GPIO GPIO_NUM_3
-#define INPUT_GPIO GPIO_NUM_5
+#define OUTPUT_GPIO GPIO_NUM_2
+#define INPUT_GPIO GPIO_NUM_3
+
+static QueueHandle_t gpio_event_queue = NULL;
+
+bool light = false;
+
+static void IRAM_ATTR gpio_isr_handler(void *args)
+{
+    uint32_t gpio_num = (uint32_t)args;
+    xQueueSendFromISR(gpio_event_queue, &gpio_num, NULL);
+}
+
+static void gpio_task(void *args)
+{
+    uint32_t pv_buffer;
+    for (;;)
+    {
+        if (xQueueReceive(gpio_event_queue, &pv_buffer, portMAX_DELAY))
+        {
+            light = !light;
+            gpio_set_level(OUTPUT_GPIO, light ? 1 : 0);
+        }
+    }
+}
 
 void app_main(void)
 {
-    gpio_set_direction(OUTPUT_GPIO, GPIO_MODE_OUTPUT);
-    // gpio_set_direction(INPUT_GPIO, GPIO_MODE_INPUT);
 
-    bool light_state = false;
+    gpio_config_t io_config = {};
+
+    io_config.intr_type = GPIO_INTR_DISABLE;
+    io_config.mode = GPIO_MODE_OUTPUT;
+    io_config.pull_down_en = 0;
+    io_config.pin_bit_mask = (1ULL << OUTPUT_GPIO);
+    io_config.pull_up_en = 0;
+    gpio_config(&io_config);
+
+    io_config.intr_type = GPIO_INTR_POSEDGE;
+    io_config.mode = GPIO_MODE_INPUT;
+    io_config.pin_bit_mask = (1ULL << INPUT_GPIO);
+    io_config.pull_up_en = 1;
+    gpio_config(&io_config);
+
+    gpio_set_intr_type(INPUT_GPIO, GPIO_INTR_ANYEDGE);
+
+    gpio_event_queue = xQueueCreate(10, sizeof(uint32_t));
+    xTaskCreate(gpio_task, "gpio_task", 2048, NULL, 10, NULL);
+
+    gpio_install_isr_service(ESP_INTR_FLAG_LEVEL1);
+    gpio_isr_handler_add(INPUT_GPIO, gpio_isr_handler, (void *)INPUT_GPIO);
+
+    gpio_set_level(OUTPUT_GPIO, 1);
 
     while (true)
     {
-        gpio_set_level(OUTPUT_GPIO, light_state);
-        light_state = !light_state;
-        vTaskDelay(100);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
