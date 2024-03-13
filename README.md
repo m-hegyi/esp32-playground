3750d7ae00e6ecba1d3aa9a1e8c4e2eb84377b08
Márió Hegyi <mario.hegyi93@gmail.com>
2024-03-13 13:53:54 +0100

    first working demo to control the led from HA

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ Some code to play around with esp32
 - chip: https://github.com/wuxx/nanoESP32-C6/blob/master/README_en.md
 - https://github.com/espressif/vscode-esp-idf-extension/blob/master/docs/tutorial/basic_use.md
 - https://docs.espressif.com/projects/esp-zigbee-sdk/en/latest/esp32/developing.html
+- https://www.nxp.com/docs/en/user-guide/JN-UG-3076.pdf
+- http://embedded.kau.ac.kr/embedded/courses/11h2wsn/075367r02ZB_AFG-Home_Automation_Profile_for_Public_Download.pdf
+
+# Useful documents
 
 - https://www.nxp.com/docs/en/user-guide/JN-UG-3076.pdf
 - http://embedded.kau.ac.kr/embedded/courses/11h2wsn/075367r02ZB_AFG-Home_Automation_Profile_for_Public_Download.pdf
+- zcl cluster library: https://zigbeealliance.org/wp-content/uploads/2019/12/07-5123-06-zigbee-cluster-library-specification.pdf
--- main/main.c
+++ main/main.c
@@ -16,6 +16,9 @@
 #error Define ZB_ED_ROLE in idf.py menuconfig to compile (End Device) source code.
 #endif
 
+char model_id[18] = "ESP32C6-light-demo";
+char manufacture_name[9] = "Espressif";
+
 static QueueHandle_t gpio_event_queue = NULL;
 
 static const char *ZB_TAG = "ZIGBEE";
@@ -117,11 +120,40 @@ void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct)
     }
 }
 
+static esp_err_t zigbee_attribute_handler(const esp_zb_zcl_set_attr_value_message_t *message)
+{
+    esp_err_t ret = ESP_OK;
+
+    ESP_RETURN_ON_FALSE(message, ESP_FAIL, ZB_TAG, "Empty message");
+    ESP_RETURN_ON_FALSE(message->info.status == ESP_ZB_ZCL_STATUS_SUCCESS, ESP_ERR_INVALID_ARG, ZB_TAG, "Received message: error status(%d)",
+                        message->info.status);
+    ESP_LOGI(ZB_TAG, "Received message: endpoint(%d), cluster(0x%x), attribute(0x%x), data size(%d)", message->info.dst_endpoint, message->info.cluster,
+             message->attribute.id, message->attribute.data.size);
+
+    if (message->info.dst_endpoint == HA_ESP_LIGHT_ENDPOINT)
+    {
+        if (message->info.cluster == ESP_ZB_ZCL_CLUSTER_ID_ON_OFF)
+        {
+            if (message->attribute.id == ESP_ZB_ZCL_ATTR_ON_OFF_ON_OFF_ID && message->attribute.data.type == ESP_ZB_ZCL_ATTR_TYPE_BOOL)
+            {
+                light = message->attribute.data.value ? *(bool *)message->attribute.data.value : light;
+                ESP_LOGI(ZB_TAG, "Light sets to %s", light ? "On" : "Off");
+                gpio_set_level(OUTPUT_GPIO, light ? 1 : 0);
+            }
+        }
+    }
+
+    return ret;
+}
+
 static esp_err_t zigbee_action_handler(esp_zb_core_action_callback_id_t callback_id, const void *message)
 {
     esp_err_t ret = ESP_OK;
     switch (callback_id)
     {
+    case ESP_ZB_CORE_SET_ATTR_VALUE_CB_ID:
+        ret = zigbee_attribute_handler(message);
+        break;
     default:
         ESP_LOGW(ZB_TAG, "Receive Zigbee action(0x%x) callback", callback_id);
         break;
@@ -141,14 +173,61 @@ void zigbee_task(void *args)
         }};
     esp_zb_init(&nwk_config);
 
-    esp_zb_on_off_light_cfg_t light_cfg = ESP_ZB_DEFAULT_ON_OFF_LIGHT_CONFIG();
-    esp_zb_ep_list_t *esp_zb_light_ep = esp_zb_on_off_light_ep_create(HA_ESP_LIGHT_ENDPOINT, &light_cfg);
-    esp_zb_on_off_switch_cfg_t switch_cfg = ESP_ZB_DEFAULT_ON_OFF_SWITCH_CONFIG();
-    esp_zb_ep_list_t *esp_zb_switch_ep = esp_zb_on_off_switch_ep_create(1, &switch_cfg);
+    uint8_t version_id = 0;
+    uint8_t zcl_version_id = 4;
 
-    esp_zb_light_ep->next = esp_zb_switch_ep;
+    esp_zb_attribute_list_t *esp_zb_basic_cluster = esp_zb_zcl_attr_list_create(ESP_ZB_ZCL_CLUSTER_ID_BASIC);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_ZCL_VERSION_ID, &version_id);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_POWER_SOURCE_ID, &zcl_version_id);
+    esp_zb_cluster_update_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_ZCL_VERSION_ID, &zcl_version_id);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_MODEL_IDENTIFIER_ID, &model_id[0]);
+    esp_zb_basic_cluster_add_attr(esp_zb_basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_MANUFACTURER_NAME_ID, &manufacture_name[0]);
 
-    esp_zb_device_register(esp_zb_switch_ep);
+    esp_zb_attribute_list_t *esp_zb_identify_cluster = esp_zb_zcl_attr_list_create(ESP_ZB_ZCL_CLUSTER_ID_IDENTIFY);
+    esp_zb_identify_cluster_add_attr(esp_zb_identify_cluster, ESP_ZB_ZCL_ATTR_IDENTIFY_IDENTIFY_TIME_ID, &version_id);
+
+    /* group cluster create with fully customized */
+    esp_zb_attribute_list_t *esp_zb_groups_cluster = esp_zb_zcl_attr_list_create(ESP_ZB_ZCL_CLUSTER_ID_GROUPS);
+    esp_zb_groups_cluster_add_attr(esp_zb_groups_cluster, ESP_ZB_ZCL_ATTR_GROUPS_NAME_SUPPORT_ID, &version_id);
+
+    /* scenes cluster create with standard cluster + customized */
+    esp_zb_attribute_list_t *esp_zb_scenes_cluster = esp_zb_scenes_cluster_create(NULL);
+    esp_zb_cluster_update_attr(esp_zb_scenes_cluster, ESP_ZB_ZCL_ATTR_SCENES_NAME_SUPPORT_ID, &version_id);
+
+    /* on-off cluster create with standard cluster config*/
+    esp_zb_on_off_cluster_cfg_t on_off_cfg;
+    on_off_cfg.on_off = ESP_ZB_ZCL_ON_OFF_ON_OFF_DEFAULT_VALUE;
+    esp_zb_attribute_list_t *esp_zb_on_off_cluster = esp_zb_on_off_cluster_create(&on_off_cfg);
+
+    /* create cluster lists for this endpoint */
+    esp_zb_cluster_list_t *esp_zb_cluster_list = esp_zb_zcl_cluster_list_create();
+    esp_zb_cluster_list_add_basic_cluster(esp_zb_cluster_list, esp_zb_basic_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+
+    /* update basic cluster in the existed cluster list */
+    // esp_zb_cluster_list_update_basic_cluster(esp_zb_cluster_list, esp_zb_basic_cluster_create(NULL), ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_identify_cluster(esp_zb_cluster_list, esp_zb_identify_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_groups_cluster(esp_zb_cluster_list, esp_zb_groups_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_scenes_cluster(esp_zb_cluster_list, esp_zb_scenes_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_on_off_cluster(esp_zb_cluster_list, esp_zb_on_off_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+
+    esp_zb_ep_list_t *esp_zb_ep_list = esp_zb_ep_list_create();
+    esp_zb_endpoint_config_t esp_zb_ep_config = {
+        .endpoint = HA_ESP_LIGHT_ENDPOINT,
+        .app_device_id = ESP_ZB_HA_ON_OFF_LIGHT_DEVICE_ID,
+        .app_profile_id = ESP_ZB_AF_HA_PROFILE_ID,
+    };
+    /* add created endpoint (cluster_list) to endpoint list */
+    esp_zb_ep_list_add_ep(esp_zb_ep_list, esp_zb_cluster_list, esp_zb_ep_config);
+    esp_zb_device_register(esp_zb_ep_list);
+
+    // esp_zb_on_off_light_cfg_t light_cfg = ESP_ZB_DEFAULT_ON_OFF_LIGHT_CONFIG();
+    // esp_zb_ep_list_t *esp_zb_light_ep = esp_zb_on_off_light_ep_create(HA_ESP_LIGHT_ENDPOINT, &light_cfg);
+    // esp_zb_on_off_switch_cfg_t switch_cfg = ESP_ZB_DEFAULT_ON_OFF_SWITCH_CONFIG();
+    // esp_zb_ep_list_t *esp_zb_switch_ep = esp_zb_on_off_switch_ep_create(1, &switch_cfg);
+
+    // esp_zb_light_ep->next = esp_zb_switch_ep;
+
+    // esp_zb_device_register(esp_zb_switch_ep);
     esp_zb_core_action_handler_register(zigbee_action_handler);
 
     esp_zb_set_primary_network_channel_set(ESP_ZB_PRIMARY_CHANNEL_MASK);
@@ -158,7 +237,6 @@ void zigbee_task(void *args)
 
 void app_main(void)
 {
-
     gpio_config_t io_config = {};
 
     io_config.intr_type = GPIO_INTR_DISABLE;
